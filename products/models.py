c291a1e82cfb019cb13de1839be776ef2496060f
Kreex <marina-denis@ya.ru>
2023-10-20 07:40:30 +0300

    Работа с моделями через консоль

--- products/models.py
+++ products/models.py
@@ -6,6 +6,10 @@ class ProductCategory(models.Model):
     description = models.TextField(null=True, blank=True)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
