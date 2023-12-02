fb8edf2d0db5bc475c1ccc6624a7afc1129ba969
XKreex <marina-denis@ya.ru>
2023-12-02 10:58:29 +0300

    Валидация группы полей

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, re_path
 
 
-from .views import forms_lable, registration, test, products, users, login
+from .views import forms_lable, registration, products, users, login #test
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('registrations/', registration, name='registration'),
-    re_path(r'^tes?', test, name='test'),
+    path('t', forms_lable),
+    #re_path(r'^tes?', test, name='test'),
     path('products/', products),
     path('products/<int:productid>/', products),
     path('', users),
-    path('t', forms_lable),
+
 
 #    path('users/<int:nameid>/<str:name>', users),
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.http import *
 from django.shortcuts import render
 
-from users.forms import UserForm, NewForm
+from users.forms import UserForm
 from users.forms import UserLoginForm
 from users.models import User
 
@@ -16,17 +16,6 @@ def registration(request):
     return render(request, 'users/registration.html')
 
 
-def test(request):
-    content = {'form': UserLoginForm}
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        output = f"<h2>Пользователь</h2><h3>Имя - {name}</h3> <h3>Возраст - {age}</h3>"
-
-        return HttpResponse(output)
-    else:
-        userform = UserForm()
-        return render(request, 'users/test.html', {'form': userform})
 def products(request, productid):
     category = request.GET.get("cat", "")
     output = "<h2>Продукт No {0} Категория: {1}</h2>".format(productid, category)
@@ -41,12 +30,27 @@ def users(request):
 
 
 def forms_lable(request):
-    # if request.method == 'POST':
-    #     name = request.POST.get('user')
-    #     password = request.POST.get('password')
-    #     bb = request.POST.get('bb')
-    #     output = f'1-{name}  2-{password}, 3={bb}'
-    #     return HttpResponse(output)
-    # else:
-        newform = NewForm()
-        return render(request, 'users/test.html', {'form': newform})
+     userform = UserForm()
+     if request.method == 'POST':
+         userform = UserForm(request.POST)
+         if userform.is_valid():
+             name =userform.cleaned_data["name"]
+             return HttpResponse(f'Имя введено корректно {name}')
+
+         # else:
+         #     return HttpResponse(f'Ошибка')
+
+     return render(request, 'users/test.html', {'form': userform})
+
+
+# def test(request):
+#     content = {'form': UserLoginForm}
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         age = request.POST.get('age')
+#         output = f"<h2>Пользователь</h2><h3>Имя - {name}</h3> <h3>Возраст - {age}</h3>"
+#
+#         return HttpResponse(output)
+#     else:
+#         userform = UserForm()
+#         return render(request, 'users/test.html', {'form': userform})
